b1b285d404f13fd3dd97a4fed94575181710ed1e
ShinobiWPS <axanderghemov@gmail.com>
2024-02-22 15:49:02 +0100

    feat: add PWA capabilities
    removed basicSSL for HTTPS on local dev env

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,54 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        theme_color: '#58AAEC',
+        icons: [
+          {
+            src: '/favicon.ico',
+            sizes: '48x48',
+            type: 'image/x-icon',
+          },
+
+          {
+            src: '/android-launchericon-48-48.png',
+            sizes: '48x48',
+            type: 'image/png',
+          },
+          {
+            src: '/64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: '/192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
